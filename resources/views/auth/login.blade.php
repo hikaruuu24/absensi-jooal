5a5f5b7b89fc62251e1d75c421f17a7d4409927e
AndiOdaag <dev@grootech.id>
2024-01-19 17:12:08 +0700

    tambah permission role sales & hidden password login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -75,6 +75,15 @@
             </div>
         </div>
 
+        <!-- JAVASCRIPT -->
+        <script src="{{asset('backend/libs/jquery/jquery.min.js')}}"></script>
+        <script src="{{asset('backend/libs/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+        <script src="{{asset('backend/libs/metismenu/metisMenu.min.js')}}"></script>
+        <script src="{{asset('backend/libs/simplebar/simplebar.min.js')}}"></script>
+        <script src="{{asset('backend/libs/node-waves/waves.min.js')}}"></script>
+        
+        <!-- App js -->
+        <script src="{{asset('backend/js/app.js')}}"></script>
     </body>
 
 </html>
